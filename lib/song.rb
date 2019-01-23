99f7475e2f67644db6500f4de7a96af39df2eb18
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-23 00:52:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,27 @@
 class Song
+  attr_accessor :name, :artist, :genre
 
-
-
-
-
-
-
-
-
-
-
+  @@count = 0 
 
   
+  def initizialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre 
+    
+  end
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
