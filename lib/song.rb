89c5f71470a7dba683e627887ada62075a05dcce
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-23 01:45:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,11 +31,12 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
-        genre_count[genre] += 1
+        genre_count[genre] += 1 
       else
         genre_count[genre] = 1
       end
     end
+    genre_count
   end
 
   def self.artist_count
